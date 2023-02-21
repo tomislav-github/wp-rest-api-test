9a5d6ea37d23cbb8bbd6cc58e36a54724a6d0865
Tomislav <tomislav@borndigital.ba>
2023-02-21 01:14:14 +0100

    Created header component

--- components/header/index.php
+++ components/header/index.php
@@ -1,3 +1,36 @@
 <style>
     <?php include 'header.style.css'; ?>
 </style>
+
+<header class="header">
+    <div class="header-nav">
+        <div class="container">
+            <div class="header-nav-item">
+                <img src="https://www.sterktechniekonderwijs.nl/wp-content/uploads/sites/2/2019/11/placeholder.com-logo1.png" alt="logo">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Products</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="header-hero">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="header-hero-item">
+                        <h1>Your Blog Title Here.</h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
