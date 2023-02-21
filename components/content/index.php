8cdb69a624efe28a8cbd598a9c995fefba3987ad
Tomislav <tomislav@borndigital.ba>
2023-02-21 01:12:09 +0100

    Created content component

--- components/content/index.php
+++ components/content/index.php
@@ -1,3 +1,11 @@
 <style>
     <?php include 'content.style.css'; ?>
 </style>
+
+<div class="content">
+    <div class="container">
+        <div class="row content-posts">
+
+        </div>
+    </div>
+</div>
