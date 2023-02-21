1a450922bba6e1c53d0147f0f1a8dc2affba9ec9
Tomislav <tomislav@borndigital.ba>
2023-02-21 01:13:11 +0100

    Created footer component

--- components/footer/index.php
+++ components/footer/index.php
@@ -1,3 +1,74 @@
 <style>
     <?php include 'footer.style.css'; ?>
 </style>
+
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="footer-item">
+                    <h2>Contact Us</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Pellentesque in ipsum</a>
+                        </li>
+                        <li>
+                            <a href="#">Proin eget tortor risus</a>
+                        </li>
+                        <li>
+                            <a href="#">Lorem ipsum dolor</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="footer-item">
+                    <h2>Links</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Pellentesque in ipsum</a>
+                        </li>
+                        <li>
+                            <a href="#">Proin eget tortor risus</a>
+                        </li>
+                        <li>
+                            <a href="#">Lorem ipsum dolor</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="footer-item">
+                    <h2>Products</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Pellentesque in ipsum</a>
+                        </li>
+                        <li>
+                            <a href="#">Proin eget tortor risus</a>
+                        </li>
+                        <li>
+                            <a href="#">Lorem ipsum dolor</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="footer-item">
+                    <h2>Follow us</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Pellentesque in ipsum</a>
+                        </li>
+                        <li>
+                            <a href="#">Proin eget tortor risus</a>
+                        </li>
+                        <li>
+                            <a href="#">Lorem ipsum dolor</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
